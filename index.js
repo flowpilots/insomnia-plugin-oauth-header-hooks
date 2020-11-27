dfa33d195a05d29921fd3e07693601aeef614873
Adem Aytac <adem.aytac@flowpilots.com>
2020-11-27 17:06:08 +0100

    performingCall to retrieve token

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// For help writing plugins, visit the documentation to get started:
-//   https://support.insomnia.rest/article/26-plugins
+const pluginName = '[oauth-header-hooks]';
 
 const STORAGE = {
   access_token: undefined,
+
+  setToken(token) {
+    STORAGE.access_token = token;
+    console.log(`${pluginName} access_token is set`);
+  }
 };
 
 const Util = {
@@ -11,6 +15,8 @@ const Util = {
     const DEFAULT_CONFIG = {
       tokenEndpoint: '/oauth/token',
       exclude: [],
+      oauthAutoCall: false,
+      oauth: {},
     };
 
     return { ...DEFAULT_CONFIG, ...env.OAUTH_HEADER_HOOKS_CONFIG };
@@ -43,34 +49,76 @@ const Util = {
       }
     }
     return false;
+  },
+
+  performCall(req) {
+    const {
+      endpoint,
+      client_id,
+      client_secret,
+      grant_type,
+      scope,
+    } = Util.getConfig(req).oauth;
+
+    const body = [
+      `client_id=${client_id}`,
+      `client_secret=${client_secret}`,
+      `grant_type=${grant_type}`,
+      `scope=${scope}`,
+    ].join('&');
+
+
+    return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          resolve(JSON.parse(xhr.responseText));
+        }
+      }
+
+      xhr.open('POST', endpoint, true);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+      console.log(`${pluginName} Getting token from ${endpoint}`);
+      xhr.send(body);
+    });
+  },
+
+  setHeader(req) {
+    console.log(`${pluginName} Authorization header added to ${req.getMethod().toUpperCase()} ${Util.href(req)}`);
+    req.setHeader('Authorization', `Bearer ${STORAGE.access_token}`);
   }
 }
 
-const requestHook = function ({ app, request }) {
+const requestHook = async ({ app, request }) => {
   const config = Util.getConfig(request);
   const excludes = [...config.exclude, config.tokenEndpoint];
 
   if (!Util.excludeUrl(request, excludes)) {
     if (STORAGE.access_token) {
-      request.setHeader('Authorization', `Bearer ${STORAGE.access_token}`);
+      Util.setHeader(request);
     } else {
-      app.alert('No Token Available', `please run ${config.tokenEndpoint} first'`);
-      // TODO:
-      // v1.1 perform the call self instead of showing alert
-      // v1.2 perform the call if { "OAUTH_HEADER_HOOKS_CONFIG.performCall": true }
+      if (config.oauthAutoCall) {
+        const response = await Util.performCall(request);
+        STORAGE.setToken(response.access_token);
+        Util.setHeader(request);
+      } else {
+        app.alert('No Token Available', `please run ${config.tokenEndpoint} first'`);
+      }
     }
   }
-}
+};
 
-const saveTokenFromResponse = function ({ request, response }) {
+const saveTokenFromResponse = ({ request, response }) => {
   const config = Util.getConfig(request);
 
   if (Util.href(request).endsWith(config.tokenEndpoint)) {
     const responseContent = Util.getBody(response);
 
-    STORAGE.access_token = responseContent.access_token;
+    STORAGE.setToken(responseContent.access_token);
   }
-}
+};
 
 module.exports = {
   requestHooks: [requestHook],
--- readme.md
+++ readme.md
@@ -13,11 +13,19 @@ Add configuration by setting OAUTH_HEADER_HOOKS_CONFIG environment variable.
 ```
 {
   "OAUTH_HEADER_HOOKS_CONFIG": {
-    "tokenEndpoint": "/oauth/token",
-    "exclude": [
+    "tokenEndpoint": "/oauth/token", // this is the token endpoint which is going to be called to retrieve token
+    "exclude": [ // endpoints in this list will be ignored. So the plugin will not add the header on them
       "/public-accesible-endpoint",
       {url: "/another-publicly-available-url", method: "post"}
-    ]
+    ],
+    "oauthAutoCall": true // setting this to true will call the tokenEndpoint if it isn't called upfront,
+    "oauth": { // contains the object to make the call
+      "endpoint": "http://api.com/oauth/token",
+      "client_id": "123",
+      "client_secret": "s3cr3t",
+      "grant_type": "client_credentials",
+      "scope": "users moderator dashboard",
+    }
   }
 }
 ```
